2cf62927578bb3e521ec68d3a8fab0d6f74a27dd
YJ.Li <56273554+jeffrey9911@users.noreply.github.com>
2024-03-05 12:54:23 -0500

    Init

---
+++ MP4StreamingServer.py
@@ -0,0 +1,58 @@
+import asyncio
+import json
+import logging
+import os
+
+from aiohttp import web
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaPlayer
+
+ROOT = os.path.dirname('/Users/yaojie/LocalRepo/MP4Streaming/sample.mp4')
+
+def create_local_tracks(play_from):
+    player = MediaPlayer(play_from)
+    return player.video 
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    # open media source
+    video = create_local_tracks("sample.mp4")
+
+    pc.addTrack(video)
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+pcs = set()
+async def on_shutdown(app):
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_post("/offer", offer)
+    web.run_app(app, host="0.0.0.0", port=8080)
