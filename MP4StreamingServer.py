0b0302347082cf052248751137ec07d7244aea1c
Yaojie Li <jeffrey.yaojie.li@gmail.com>
2024-03-10 23:33:00 -0400

    MPEG DASH

--- MP4StreamingServer.py
+++ MP4StreamingServer.py
@@ -1,58 +1,20 @@
-import asyncio
-import json
-import logging
+from flask import Flask, send_from_directory
 import os
 
-from aiohttp import web
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer
+app = Flask(__name__)
 
-ROOT = os.path.dirname('/Users/yaojie/LocalRepo/MP4Streaming/sample.mp4')
+# ffmpeg -i input.mp4 -map 0 -map 0 -map 0 -b:v:0 500k -b:v:1 1000k -b:v:2 2000k -s:v:1 640x360 -s:v:2 1280x720 -adaptation_sets "id=0,streams=v id=1,streams=a" -use_template 1 -use_timeline 1 -f dash output.mpd
 
-def create_local_tracks(play_from):
-    player = MediaPlayer(play_from)
-    return player.video 
+MEDIA_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
+print(MEDIA_FOLDER)
 
+@app.route('/')
+def index():
+    return 'DASH Streaming Server'
 
-async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
-    return web.Response(content_type="text/html", text=content)
+@app.route('/video/<path:path>')
+def stream_video(path):
+    return send_from_directory(MEDIA_FOLDER, path)
 
-
-async def offer(request):
-    params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    # open media source
-    video = create_local_tracks("sample.mp4")
-
-    pc.addTrack(video)
-
-    await pc.setRemoteDescription(offer)
-
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-
-    return web.Response(
-        content_type="application/json",
-        text=json.dumps(
-            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-        ),
-    )
-
-pcs = set()
-async def on_shutdown(app):
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
-    app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-    app.router.add_get("/", index)
-    app.router.add_post("/offer", offer)
-    web.run_app(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
